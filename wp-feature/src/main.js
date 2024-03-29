4731b43b3a087cb1b82d036fd990a096ef1dc862
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-03-29 17:09:49 +0600

    update configureWebpack config

--- wp-feature/src/main.js
+++ wp-feature/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+// const RemoteButton = () => import('remote/RemoteButton');
+// Vue.component('RemoteButton', RemoteButton);
+
 new Vue({
     render: (h) => h(App)
 }).$mount('#app');
--- wp-feature/vue.config.js
+++ wp-feature/vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service');
+const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            new ModuleFederationPlugin({
+                name: 'host',
+                filename: 'remoteEntry.js',
+                exposes: {
+                    './HelloWorld': './src/components/HelloWorld'
+                },
+                remotes: {
+                    remoteApp: 'remoteApp@https://micro-frontend-vue.vercel.app/assets/remoteEntry.js'
+                }
+            })
+        ]
+    },
+
+    devServer: {
+        port: 8000
+    }
 });
