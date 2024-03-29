b8f2c1ab7b11ea2af2fd66b82c55fbd2b2fbe651
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-03-29 15:10:26 +0600

    Delete MButton.vue

--- vite-module/src/components/MButton.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    const onClick = () => {
-        console.log('Button clicked from module-02');
-    };
-</script>
-
-<template>
-    <button @click="onClick">Click me</button>
-</template>
